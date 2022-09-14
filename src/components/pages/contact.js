86f0c38c00fd66f0c67de92e015b64293cc335ba
Eric Mance Courtney <109315939+Cire1981@users.noreply.github.com>
2022-09-14 12:15:55 -0400

    Update

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,5 +1,45 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
 
 export default function() {
-    return <div>Contact</div>
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">409-370-0289</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">emc81185776@gmail.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Fenton,Mi</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
